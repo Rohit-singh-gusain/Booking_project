93a0116d45de1974cf53469f2fcd1e536cd11914
Christos Bandis <chr.bandis@gmail.com>
2022-11-27 12:38:14 +0200

    Update admin-movies-header.php

--- includes/admin-movies-header.php
+++ includes/admin-movies-header.php
@@ -1,7 +1,7 @@
 <?php
-    session_start(); //Εκκίνηση του session
+    session_start(); //Start the session
 
-    if (!isset($_SESSION['loggedin'])) { //Αν δεν έχει πραγματοποιηθεί σύνδεση χρήστη τότε πραγματοποιείται ανακατεύθυνση στο login.php
+    if (!isset($_SESSION['loggedin'])) { //If no user login has been made then redirect to login.php
         header('Location: login.php');
     }
 ?>
@@ -10,7 +10,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Admin Dashboard</title>
         <link rel="icon" type="image/png" href="../img/logo.png">
         <link rel="shortcut icon" href="../images/a-master-favicon.ico">
         <link rel="stylesheet" href="../css/admin-panel.css">
@@ -19,7 +18,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
 <body>
-<div class="admin-section-header"> <!-- Div για τo header -->
+<div class="admin-section-header"> <!-- Header -->
         <div class="admin-logo">
             <img src="../images/logo.png" class="logo">
         </div>
@@ -29,8 +28,8 @@
             
         </div>
 </div>
-<div class="admin-container"> <!-- Div για τα περιεχόμενα της σελίδας (κλείνει στο τέλους του movies.php) -->
-    <div class="admin-section admin-section3"> <!-- Div για το side menu -->
+<div class="admin-container"> <!-- Div for page contents (closes at the end of movies.php) -->
+    <div class="admin-section admin-section3"> <!-- for side menu -->
         <ul>
             <a href="index.php"><li><i class="fas fa-sliders-h"></i>Dashboard </li></a>
             <a href="bookings.php"><li><i class="fas fa-ticket-alt"></i>Bookings</li></a>
@@ -38,7 +37,7 @@
             <a id="logout" href="logout.php"><li><i class="fas fa-sign-out-alt"></i>Logout</li></a>
         </ul>
         <script>
-        $(function(){ //JQUERY που εμφανίζει μήνυμα επιβεβαίσης όταν ο χρήστης εκτελεί μία συσκεκριμένη ενέργεια (αποσύνδεση)
+        $(function(){ //JQUERY showing confirmation message when user performs a concreted action (logout)
             $('a#logout').click(function(){
                 if(confirm('Are you sure you want to logout?')) {
                     return true;
