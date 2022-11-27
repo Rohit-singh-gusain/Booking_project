43b436017d92d764a64846a83c35d1c8bafcaed7
Christos Bandis <chr.bandis@gmail.com>
2022-11-27 13:23:42 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,43 +1,43 @@
 <?php
-require '../database/Db.class.php'; //Κλήση του απαραίτητου για την σύνδεση με τη βάση δεδομένων και την εκτέλεση ερωτημάτων αρχείου κλάσης Db.class.php
-require '../includes/admin-header.php'; //Κλήση του απαραίτητου για την εκτέλεση του παρακάτω κώδικα αρχείου, header.php
+require '../database/Db.class.php'; //Call the necessary class file (Db.class.php) to connect to the database and run queries
+require '../includes/admin-header.php'; //Call the file necessary to execute the following code, admin-header.php
 
-$db = new Db(); //Δημιουργία object της κλάσης Db
+$db = new Db(); //Create a Db class object
 
-$getThreeBookings = $db->get_three_bookings_and_title(); //Αποθήκευση του αποτελέσματος της function get_three_bookings_and_title() της κλάσης Db στη μεταβλητή $getThreeBookings
-$bookings=$db->get_movies_num(); //O συνολικός αριθμός των εγγραφών της function get_all_movies() της κλάσης Db  εισάγεται στη μεταβλητή $bookings
-$movies=$db->get_bookings_num(); //O συνολικός αριθμός των εγγραφών της function get_all_bookings() της κλάσης Db  εισάγεται στη μεταβλητή $movies
+$getThreeBookings = $db->get_three_bookings_and_title(); //Save the result of function get_three_bookings_and_title() of class Db to the variable $getThreeBookings
+$bookings=$db->get_movies_num(); //The total number of records in function get_all_movies() in class Db is stored into the $bookings variable
+$movies=$db->get_bookings_num(); //the total number of records in function get_all_bookings() in class db is stored into the $movies variable
 
-function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert που εμφανίζει ένα javascript alert που περιέχει ένα μήνυμα και στη συνέχεια ανακατευθύνει
+function phpAlert($msg) { //Create the phpAlert method that displays a javascript alert containing a message and then redirects
 	echo '<script type="text/javascript">alert("' . $msg . '");window.location = \'bookings.php\';</script>';
 }
 
 ?>
     
-    <div class="admin-section admin-section2"> <!-- Div για το περιεχόμενο της σελίδας -->
+    <div class="admin-section admin-section2"> <!-- Div for page content -->
         <div class="admin-section-column">
-            <div class="admin-section-panel admin-section-stats"> <!-- Div για τα στατιστικά -->
+            <div class="admin-section-panel admin-section-stats"> <!-- Div for stats -->
                 <div class="admin-section-stats-panel">
                     <i class="fas fa-check-square" style="background-color: #432e8d"></i>
-                    <h2 style="color: #432e8d"><?php echo $bookings ?></h2> <!-- Εμφάνιση των συνόλου των εγγραφών του πίνακα t_booking -->
+                    <h2 style="color: #432e8d"><?php echo $bookings ?></h2> <!-- Display the set of records in the t_booking table -->
                     <h3>Bookings</h3>
                 </div>
                 <div class="admin-section-stats-panel">
                     <i class="fas fa-film" style="background-color: #612dca"></i>
-                    <h2 style="color: #612dca"><?php echo $movies ?></h2> <!-- Εμφάνιση των συνόλου των εγγραφών του πίνακα t_movies -->
+                    <h2 style="color: #612dca"><?php echo $movies ?></h2> <!-- Display all of the records in the t_movies table -->
                     <h3>Movies</h3>
                 </div>
-                <div class="admin-section-stats-panel" style="padding-top:10px; cursor: pointer;" onclick="document.getElementById('popup').style.display='block'"> <!-- Όταν ο χρήστης κάνει κλικ στο div εκτελείται κώδικας javascript -->
+                <div class="admin-section-stats-panel" style="padding-top:10px; cursor: pointer;" onclick="document.getElementById('popup').style.display='block'"> <!-- When the user clicks the div, javascript code runs -->
                     <i class="fas fa-book" style="background-color: #812dd6"></i>
                     <h3>Create a Booking</h3>
                     
                 </div>
-                <div class="admin-section-stats-panel" style="padding-top:10px; cursor: pointer; border:0;" onclick="window.location='../index.php';"> <!-- Όταν ο χρήστης κάνει κλικ στο div ανακατευθύνεται στην αρχική σελίδα -->
+                <div class="admin-section-stats-panel" style="padding-top:10px; cursor: pointer; border:0;" onclick="window.location='../index.php';"> <!-- When the user clicks on the div they are redirected to the home page -->
                     <i class="fas fa-door-open" style="background-color: #b91a59"></i>
                     <h3>Visit Website</h3>
                 </div>
             </div>
-            <div class="admin-section-panel admin-section-panel1"> <!-- Div για τα bookings -->
+            <div class="admin-section-panel admin-section-panel1"> <!-- Div for bookings -->
                 <div class="admin-panel-section-header">
                     <h2>Bookings</h2>
                 </div>
@@ -45,7 +45,7 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
                     <?php
                     
                         if(mysqli_num_rows($getThreeBookings) > 0){
-                            while($row = mysqli_fetch_array($getThreeBookings)){ //Ανάθεση των περιεχωμένων της μεταβλητής $getThreeBookings στη μεταβλητή $row και εμφάνιση των ζητούμενων αποτελεσμάτων όσο αυτή περιέχει τιμές
+                            while($row = mysqli_fetch_array($getThreeBookings)){ //Assigning the contents of the $getThreeBookings variable to the $row variable and displaying the requested resultsν όσο αυτή περιέχει τιμές
                                 echo "<div class=\"admin-panel-section-booking-item\" style=\"margin: 10px 10px;\">\n";
                                         echo "      <div class=\"admin-panel-section-booking-info\">\n";
                                         echo "            <div>\n";
@@ -70,7 +70,7 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
                                         echo "                  <h4>Booking Date: ". $row['bookTimestamp'] ."</h4>\n";
                                         echo "            </div>\n";
                                         echo "      </div>\n";
-                                        echo "      <div class=\"admin-panel-section-booking-response\">\n";   //Επιλογή για διαγραφή κράτησης
+                                        echo "      <div class=\"admin-panel-section-booking-response\">\n";   //Option to delete a booking
                                         echo "            <a id='delbook' href='../includes/deleteBooking.php?id=".$row['bookingID']."'><i class=\"fas fa-times delete-icon\" style=\"height: 155px; line-height:150px;\" title=\"Delete booking\"></i></a>\n";
                                         echo "      </div>\n";
                                         echo "</div>";
@@ -82,15 +82,15 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
                     ?>
                     <a href="bookings.php"><button type="submit" value="submit" name="submit" style="margin-left:25%;" class="view-all-btn">View all bookings</button></a>
                 </div>                
-            </div> <!--  Τέλος div για τα bookings -->
+            </div> <!-- End of div for bookings -->
 
                 
 
-            <div class="admin-section-panel admin-section-panel2"> <!-- Div για τη φόρμα προσθήκης ταινίας -->
+            <div class="admin-section-panel admin-section-panel2"> <!-- Div for the movie add form -->
                 <div class="admin-panel-section-header">
                     <h2>Add Movie</h2>
                 </div>
-                <form action="../includes/add-movie.php" method="POST" enctype="multipart/form-data"> <!-- Φόρμα προσθήκης ταινίας -->
+                <form action="../includes/add-movie.php" method="POST" enctype="multipart/form-data"> <!-- Add a movie form -->
                     <input placeholder="Title" type="text" name="movieTitle" required>
                     <input placeholder="Description (max 500 chars)" type="text" name="movieDescription" required>
                     <input placeholder="Genre" type="text" name="movieGenre" required>
@@ -109,12 +109,12 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
                         <input type="file" name="movieCover" id="moviebtn">
                     </div>
                     <button type="submit" value="submit" name="submit" class="form-btn">Add Movie</button>  
-                </form> <!--  Τέλος φόρμας προσθήκης ταινίας -->
+                </form> <!--  End of the movie add form -->
             </div>
         </div>
-    </div> <!-- Κλείσιμο div για το περιεχόμενο της σελίδας -->
+    </div> <!-- End of div for page content -->
 
-    <div id="popup" class="popup-dialog"> <!-- Div για τη δημιουργία κράτησης που εμφανίζεται όταν ο χρήστης κάνει κλικ στο div που δημιουργείται στη γραμμή 29 -->    
+    <div id="popup" class="popup-dialog"> <!-- Div to create booking that appears when the user clicks on the div generated in line 29 -->    
         <div class="admin-section-panel admin-section-panel2 animate" style="margin:20% auto 0 auto; width:70%; height:30%;">
                     <div class="admin-panel-section-header">
                         <h2>Create Booking</h2>
@@ -124,7 +124,7 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
                         <?php
                            
                             $getAllMovies = $db->get_all_movies();
-                            while ($row = mysqli_fetch_array($getAllMovies)) {  //Όσο υπάρχουν αποτελέσματα στη μεταβλητή $getAllMovies, αυτά εισάγονται στη μεταβλητή $row και εμφανίζονται στα options του select field της φόρμας
+                            while ($row = mysqli_fetch_array($getAllMovies)) {  //As long as there are results in the $getAllMovies variable, they are stored into the $row variable and appear in the select field options of the form
                                 echo "<option value=" . $row['movieTitle'] .">". $row['movieTitle'] ."</option>";
                             }
                         ?>
@@ -146,11 +146,11 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
 
 
 
-    if(isset($_POST['submit'])) //Έλεγχος αν έχει γίνει υποβολή των στοιχείων στη φόρμα κράτησης
+    if(isset($_POST['submit'])) //Check if the details have been submitted to the booking form
     {
-        $movTitle = $_POST["movieTitle"]; //Ανάθεση της επιλεγμένης τιμής (value) των παραπάνω options στη μεταβλητή $movieTitle
+        $movTitle = $_POST["movieTitle"]; //Assign the selected value of the above options to the variable $movieTitle
         $row2 = $db->get_movie_byTitle($movTitle);                         
-                                $id = $row2['movieID']; //Ανάθεση του id της επιλεγμένης εγγραφής στη μεταβλητή $id
+                                $id = $row2['movieID']; //Assign the id of the selected record to the variable $id
                                 $fname = $_POST["fname"];
                                 $lname = $_POST["lname"];
                                 $email = $_POST["email"];
@@ -163,19 +163,19 @@ function phpAlert($msg) { //Δημιουργία της μεθόδου phpAlert 
                                 $db->create_booking("$id", "$fname", "$lname", "$email", "$phone", "$date", "$hour", "$row", "$col", "admin");
                                 phpAlert("Your booking for the movie " . $movTitle . " was successful!");                                        
                             }
-    $db->close(); //Τερματισμός της σύνδεσης με τη βάση   
+    $db->close(); //End the connection to the database  
     ?>
-    <script> //Javascript για τη σωστή εκτέλεση της εμφάνισης του div που δημιουργείται στη γραμμή 118   
+    <script> //Javascript to correctly execute the display of the div generated in line 118  
     var popup = document.getElementById('popup');
 
     
-    window.onclick = function(event) { // Όταν γίνει κλικ εκτός του div τότε αυτό κλείνει
+    window.onclick = function(event) { //when it is clicked outside the div then it closes
         if (event.target == popup) {
             popup.style.display = "none";
         }
     }
 
-    $(function(){ //JQUERY που εμφανίζει μήνυμα επιβεβαίσης όταν ο χρήστης εκτελεί μία συσκεκριμένη ενέργεια (αποσύνδεση)
+    $(function(){ //JQUERY showing confirmation message when user performs a concreted action (delete booking)
             $('a#delbook').click(function(){
                 if(confirm('Are you sure you want to delete this booking?')) {
                     return true;
